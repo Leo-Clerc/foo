e38870f8ae47de332295dbda2f4a6818c922c47f
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2023-01-07 14:15:31 +0100

    Update Foo.java

--- src/main/java/ei2/foo/Foo.java
+++ src/main/java/ei2/foo/Foo.java
@@ -12,6 +12,6 @@ package ei2.foo;
 public class Foo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! This is a test");
     }
 }
